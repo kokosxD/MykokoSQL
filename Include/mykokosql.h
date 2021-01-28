8f6ffa6cb12ee906f6357ae35d50a3aa352b6e05
kokosxD <kokos1811@outlook.com.gr>
2021-01-28 15:13:29 +0200

    Add connection functions

--- Include/mykokosql.h
+++ Include/mykokosql.h
@@ -8,6 +8,25 @@
 
 // Class used to represent a connection to a MySQL database
 class MykokoSQL final{
+private:
+	MYSQL* m_db = nullptr;
 public:
 	MykokoSQL() noexcept;
+
+	~MykokoSQL() noexcept;
+
+	// Returns true if MySQL is initialized
+	const bool Init() noexcept;
+
+	// Closes the current connection
+	void Close() noexcept;
+
+	// Returns true if a connection to the requested MySQL server is established
+	const bool Connect(
+		const char* const _host,
+		const char* const _user,
+		const char* const _password,
+		const char* const _db = nullptr,
+		const unsigned int& _port = 0
+	) noexcept;
 };
--- src/mykokosql.cpp
+++ src/mykokosql.cpp
@@ -1,3 +1,34 @@
 #include "mykokosql.h"
 
-MykokoSQL::MykokoSQL() noexcept{}
+MykokoSQL::MykokoSQL() noexcept{
+	Init();
+}
+
+MykokoSQL::~MykokoSQL() noexcept{
+	Close();
+}
+
+const bool MykokoSQL::Init() noexcept{
+	if(m_db){
+		m_db = mysql_init(nullptr);
+	}
+
+	return m_db;
+}
+
+void MykokoSQL::Close() noexcept{
+	if(m_db){
+		mysql_close(m_db);
+		m_db = nullptr;
+	}
+}
+
+const bool MykokoSQL::Connect(
+	const char* const _host,
+	const char* const _user,
+	const char* const _password,
+	const char* const _db,
+	const unsigned int& _port
+) noexcept{
+	return mysql_real_connect(m_db, _host, _user, _password, _db, _port, nullptr, 0);
+}
